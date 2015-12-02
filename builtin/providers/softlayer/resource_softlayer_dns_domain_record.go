45d37d885fa46cbc609e2b46dd5c124f715d8069
Eduard Bakaev <bakaeved@gmail.com>
2015-12-02 16:01:29 +0300

    #8 removed unnecessary methods as they cannot be used by terraform. fixes

--- builtin/providers/softlayer/resource_softlayer_dns_domain_record.go
+++ builtin/providers/softlayer/resource_softlayer_dns_domain_record.go
@@ -114,23 +114,6 @@ func resourceSoftLayerDnsDomainResourceRecordCreate(d *schema.ResourceData, meta
 	return resourceSoftLayerDnsDomainResourceRecordRead(d, meta)
 }
 
-
-//  Creates array of DNS Domain Resource Records
-//  https://sldn.softlayer.com/reference/services/SoftLayer_Dns_Domain_ResourceRecord/createObjects
-func resourceSoftLayerDnsDomainResourceRecordsCreate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*Client).dnsDomainResourceRecord
-	if client == nil {
-		return fmt.Errorf("The client was nil.")
-	}
-
-	log.Println("CreateDNS Record!")
-	//	guest, err := client.CreateObject(nil)
-
-	//TODO will return several records
-	return resourceSoftLayerDnsDomainResourceRecordRead(d, meta)
-}
-
-
 //  Reads DNS Domain Resource Record from SL system
 //  https://sldn.softlayer.com/reference/services/SoftLayer_Dns_Domain_ResourceRecord/getObject
 func resourceSoftLayerDnsDomainResourceRecordRead(d *schema.ResourceData, meta interface{}) error {
@@ -186,23 +169,3 @@ func resourceSoftLayerDnsDomainResourceRecordDelete(d *schema.ResourceData, meta
 
 	return nil
 }
-
-//  Deletes DNS Domain Resource Records in SL system
-//  https://sldn.softlayer.com/reference/services/SoftLayer_Dns_Domain_ResourceRecord/deleteObjects
-func resourceSoftLayerDnsDomainResourceRecordsDelete(d *schema.ResourceData, meta interface{}) error {
-	log.Println("DeleteDNS Record!")
-	client := meta.(*Client).dnsDomainResourceRecord
-	id, err := strconv.Atoi(d.Id())
-	if err != nil {
-		return fmt.Errorf("Not a valid ID, must be an integer: %s", err)
-	}
-
-	_, err = client.DeleteObject(id)
-
-	if err != nil {
-		return fmt.Errorf("Error deleting dns domain resource records: %s", err)
-	}
-
-	return nil
-}
-
