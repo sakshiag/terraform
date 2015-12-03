10de15113b75a2e8b5ca759a710d9ca321822017
Eduard Bakaev <bakaeved@gmail.com>
2015-12-03 19:36:51 +0300

    #8 dns record service renamed + tests for create are added

--- builtin/providers/softlayer/resource_softlayer_dns_domain_record.go
+++ builtin/providers/softlayer/resource_softlayer_dns_domain_record.go
@@ -25,7 +25,7 @@ func resourceSoftLayerDnsDomainResourceRecord() *schema.Resource {
 //  Creates DNS Domain Resource Record
 //  https://sldn.softlayer.com/reference/services/SoftLayer_Dns_Domain_ResourceRecord/createObject
 func resourceSoftLayerDnsDomainResourceRecordCreate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*Client).dnsDomainResourceRecord
+	client := meta.(*Client).dnsDomainResourceRecordService
 	if client == nil {
 		return fmt.Errorf("The client was nil.")
 	}
@@ -62,7 +62,7 @@ func resourceSoftLayerDnsDomainResourceRecordCreate(d *schema.ResourceData, meta
 //  Reads DNS Domain Resource Record from SL system
 //  https://sldn.softlayer.com/reference/services/SoftLayer_Dns_Domain_ResourceRecord/getObject
 func resourceSoftLayerDnsDomainResourceRecordRead(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*Client).dnsDomainResourceRecord
+	client := meta.(*Client).dnsDomainResourceRecordService
 	id, err := strconv.Atoi(d.Id())
 	if err != nil {
 		return fmt.Errorf("Not a valid ID, must be an integer: %s", err)
@@ -92,7 +92,7 @@ func resourceSoftLayerDnsDomainResourceRecordRead(d *schema.ResourceData, meta i
 //  Updates DNS Domain Resource Record in SL system
 //  https://sldn.softlayer.com/reference/services/SoftLayer_Dns_Domain_ResourceRecord/editObject
 func resourceSoftLayerDnsDomainResourceRecordUpdate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*Client).dnsDomainResourceRecord
+	client := meta.(*Client).dnsDomainResourceRecordService
 
 	recordId, _ := strconv.Atoi(d.Id())
 
@@ -146,7 +146,7 @@ func resourceSoftLayerDnsDomainResourceRecordUpdate(d *schema.ResourceData, meta
 //  https://sldn.softlayer.com/reference/services/SoftLayer_Dns_Domain_ResourceRecord/deleteObject
 func resourceSoftLayerDnsDomainResourceRecordDelete(d *schema.ResourceData, meta interface{}) error {
 	log.Println("DeleteDNS Record!")
-	client := meta.(*Client).dnsDomainResourceRecord
+	client := meta.(*Client).dnsDomainResourceRecordService
 	id, err := strconv.Atoi(d.Id())
 	if err != nil {
 		return fmt.Errorf("Not a valid ID, must be an integer: %s", err)
