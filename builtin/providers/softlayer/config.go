e0f14d451d535ace622ac2d852c87d83c580b222
Mark Kuznetsov <markuzet@gmail.com>
2015-12-16 13:03:19 +0300

    #7 implement resource_softlayer_dns_domain

---
+++ builtin/providers/softlayer/config.go
@@ -0,0 +1,36 @@
+package softlayer
+
+import (
+	"log"
+
+	slclient "github.com/maximilien/softlayer-go/client"
+	softlayer "github.com/maximilien/softlayer-go/softlayer"
+)
+
+type Config struct {
+	Username string
+	ApiKey string
+}
+
+type Client struct {
+	productOrderService softlayer.SoftLayer_Product_Order_Service
+	dnsDomainService 		softlayer.SoftLayer_Dns_Domain_Service
+}
+
+func (c *Config) Client() (*Client, error) {
+	slc := slclient.NewSoftLayerClient(c.Username, c.ApiKey)
+
+	dnsDomainService, err := slc.GetSoftLayer_Dns_Domain_Service()
+
+	if err != nil {
+		return nil, err
+	}
+
+	client := &Client {
+		dnsDomainService : 		dnsDomainService,
+	}
+
+	log.Println("[INFO] Created SoftLayer client")
+
+	return client, nil
+}
