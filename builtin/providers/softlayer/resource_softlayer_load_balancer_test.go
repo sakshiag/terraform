a4e552c229200aac070c546a9ef128f0ffde51fd
Tim Mulhern <tim.mulhern.contractor@weather.com>
2016-06-21 17:14:19 -0400

    Add basic tests for lb, lb service group, lb service resources

--- builtin/providers/softlayer/resource_softlayer_load_balancer_test.go
+++ builtin/providers/softlayer/resource_softlayer_load_balancer_test.go
@@ -1,18 +1,11 @@
 package softlayer
 
 import (
-	"fmt"
-	"strconv"
-	"testing"
-
-	datatypes "github.com/TheWeatherCompany/softlayer-go/data_types"
 	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
+	"testing"
 )
 
 func TestAccSoftLayerLoadBalancer_Basic(t *testing.T) {
-	var lb datatypes.SoftLayer_Load_Balancer
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
@@ -20,48 +13,16 @@ func TestAccSoftLayerLoadBalancer_Basic(t *testing.T) {
 			resource.TestStep{
 				Config: testAccCheckSoftLayerLoadBalancerConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckSoftLayerLoadBalancerExists("softlayer_ssh_key.testacc_foobar", &lb),
 					resource.TestCheckResourceAttr(
-						"softlayer_load_balancer.testacc_foobar_lb", "connections", 15000),
+						"softlayer_load_balancer.testacc_foobar_lb", "connections", "15000"),
 					resource.TestCheckResourceAttr(
 						"softlayer_load_balancer.testacc_foobar_lb", "location", "tok02"),
 					resource.TestCheckResourceAttr(
-						"softlayer_load_balancer.testacc_foobar_lb", "ha_enabled", false),
+						"softlayer_load_balancer.testacc_foobar_lb", "ha_enabled", "false"),
 				),
 			},
 		},
 	})
-}
-
-func testAccCheckSoftLayerLoadBalancerExists(n string, lb *datatypes.SoftLayer_Load_Balancer) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-
-		if !ok {
-			return fmt.Errorf("Not found: %s", n)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("No Record ID is set")
-		}
-
-		lbId, _ := strconv.Atoi(rs.Primary.ID)
-
-		client := testAccProvider.Meta().(*Client).loadBalancerService
-		found, err := client.GetObject(lbId)
-
-		if err != nil {
-			return err
-		}
-
-		if strconv.Itoa(int(found.Id)) != rs.Primary.ID {
-			return fmt.Errorf("Record not found")
-		}
-
-		*lb = found
-
-		return nil
-	}
 }
 
 const testAccCheckSoftLayerLoadBalancerConfig_basic = `
