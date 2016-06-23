6264f0580677688fc894823ad1ad886ce95b205f
Renier Morales <renierm@us.ibm.com>
2016-06-23 16:51:07 -0400

    Re-remove softlayer security cert resource after rebase. To be added later

--- builtin/providers/softlayer/provider.go
+++ builtin/providers/softlayer/provider.go
@@ -30,7 +30,6 @@ func Provider() terraform.ResourceProvider {
 			"softlayer_network_application_delivery_controller": resourceSoftLayerNetworkApplicationDeliveryController(),
 			"softlayer_network_loadbalancer_virtualipaddress":   resourceSoftLayerNetworkLoadBalancerVirtualIpAddress(),
 			"softlayer_network_loadbalancer_service":            resourceSoftLayerNetworkLoadBalancerService(),
-			"softlayer_security_certificate":                    resourceSoftLayerSecurityCertificate(),
 		},
 
 		ConfigureFunc: providerConfigure,
