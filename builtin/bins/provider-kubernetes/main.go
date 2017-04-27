01af5d35423219deb4523abda9fd7d7d7612395f
Ashish K Thakur <ashishth@in.ibm.com>
2017-04-27 14:54:23 +0530

    Add version to the provider

--- builtin/bins/provider-kubernetes/main.go
+++ builtin/bins/provider-kubernetes/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/hashicorp/terraform/builtin/providers/kubernetes"
 	"github.com/hashicorp/terraform/plugin"
 )
 
 func main() {
+	log.Println("IBM Cloud Provider Kubernetes version", "tf-v0.9.3-ibm-k8s-v0.1")
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: kubernetes.Provider,
 	})
