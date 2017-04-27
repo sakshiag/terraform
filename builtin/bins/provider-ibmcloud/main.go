c7c4ade2f0f74ba3b829d524c2b9866a5dfc7cf9
Ashish K Thakur <ashishth@in.ibm.com>
2017-04-27 14:50:37 +0530

    Add version to the provider

--- builtin/bins/provider-ibmcloud/main.go
+++ builtin/bins/provider-ibmcloud/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/hashicorp/terraform/builtin/providers/ibmcloud"
 	"github.com/hashicorp/terraform/plugin"
 )
 
 func main() {
+	log.Println("IBM Cloud Provider version", "tf-v0.9.3-ibm-provider-v0.1")
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: ibmcloud.Provider,
 	})
