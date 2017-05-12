840c7c83cd653ba08835f1f9de961136ac225363
Ashish K Thakur <ashishth@in.ibm.com>
2017-05-12 12:14:08 +0530

    Update the plugin version at build time

--- builtin/bins/provider-ibmcloud/main.go
+++ builtin/bins/provider-ibmcloud/main.go
@@ -7,8 +7,12 @@ import (
 	"github.com/hashicorp/terraform/plugin"
 )
 
+//Version of this provider plugin
+//go build -ldflags "-X main.Version=<version>" github.com/hashicorp/terraform/builtin/bins/provider-ibmcloud will populate the Version
+var Version = "dev"
+
 func main() {
-	log.Println("IBM Cloud Provider version", "tf-v0.9.3-ibm-provider-v0.1")
+	log.Println("IBM Cloud Provider version", Version)
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: ibmcloud.Provider,
 	})
